976bdd21a6bf0f60b1190dea05b52ae9f41ace1b
Asr-andre <suport.info.log@gmail.com>
2025-02-25 18:03:07 -0300

    🔥 retirado componente desnecessarios e incluido uma nova estrutura pra organizar o progeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgPipesModule } from 'ngx-pipes';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-export function createTranslateLoader(http: HttpClient): any {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppComponent
     ],
-    bootstrap: [AppComponent], imports: [TranslateModule.forRoot({
-            defaultLanguage: 'en',
-            loader: {
-                provide: TranslateLoader,
-                useFactory: (createTranslateLoader),
-                deps: [HttpClient]
-            }
-        }),
+    bootstrap: [AppComponent],
+    imports: [
         BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
         StoreDevtoolsModule.instrument({
-            maxAge: 25, 
-            logOnly: environment.production, 
+            maxAge: 25,
+            logOnly: environment.production,
         }),
 
         NgPipesModule], providers: [
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+            provideHttpClient(withInterceptorsFromDi()),
+        ]
+})
 export class AppModule { }
